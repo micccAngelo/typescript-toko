a58abca7f1e24fcb4d487bc51b7867edf3ce22b6
micccAngelo <michaelangelo0109@gmail.com>
2023-04-12 12:27:26 +0700

    benerin add product

--- src/API Services/AddProductAPI.tsx
+++ src/API Services/AddProductAPI.tsx
@@ -4,10 +4,7 @@ import Product from '../Model/Product';
 const AddProducts = async (values: Product): Promise<Product> => {
   try {
       console.log('values:', values);
-      const product = new Product(values);
-      const response = await BaseURL.post('/products/add', {
-          "values": product,
-      });
+      const response = await BaseURL.post('/products/add', values,);
       const apiProduct = response.data;
       console.log('apiProduct:', apiProduct);
       const productResponse = new Product(apiProduct);
